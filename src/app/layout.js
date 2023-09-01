57be38f0ae0707220c47ac79f7341b024ae249bc
Mohammed Juzer <mohammedjuzer10@gmail.com>
2023-09-01 11:32:38 +0530

    Changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,8 +7,10 @@ import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Infinite Hydraulics Solutions | Leading Hydraulic Solution provider in Chennai | Best Hydraulic Solution provider in Chennai",
-  description: "Your Trusted Hydraulic Solutions Provider | High-Quality Hoses, Fittings & More | Experts in Fluid Power Technology | Serving Chennai",
+  title:
+    "Infinite Hydraulics Solutions | Leading Hydraulic Solution provider in Chennai | Best Hydraulic Solution provider in Chennai",
+  description:
+    "Your Trusted Hydraulic Solutions Provider | High-Quality Hoses, Fittings & More | Experts in Fluid Power Technology | Serving Chennai",
 };
 
 export default function RootLayout({ children }) {
@@ -20,7 +22,25 @@ export default function RootLayout({ children }) {
             href="https://unpkg.com/swiper/swiper-bundle.min.css"
             rel="stylesheet"
           />
-          <link rel="shortcut icon" href="/Logo.png" type="image/x-icon" sizes="16x16" />
+          {/* <link rel="shortcut icon" href="/Logo.png" type="image/x-icon" sizes="16x16" /> */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicon//site.webmanifest" />
           <Navbar />
           {children}
           <Footer />
